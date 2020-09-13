480c622fe909519536ebf8ce26bd9f81afaf4781
Alexander Svilarov <aleksander.svilarovv@gmail.com>
2020-09-13 23:23:03 +0300

    Adding Game First Time

---
+++ Survival Horror/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public new Light light;
+    public GameObject flashlight;
+    void Start()
+    {
+        light.GetComponent<Light>();
+        light.enabled = false;
+    }
+    void Update()
+    {
+        //flashlight.transform.position = Camera.main.transform.position + Vector3.right * 1.2f + Vector3.forward * 1.43f;
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (light.enabled == false)
+            {
+                light.enabled = true;
+            }
+            else if(light.enabled == true)
+            {
+                light.enabled = false;
+            }
+        }
+    }
+
+}
---
+++ Survival Horror/Assets/Scripts/flicker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flicker : MonoBehaviour
+{
+    Light light;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+        StartCoroutine(flickingDelay());
+    }
+
+    public IEnumerator flickingDelay()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(0.08f, 0.55f));
+            light.enabled = !light.enabled;
+        }
+    }
+}
