3f33f6f640ddeedcb0c2806499dce2094aa87116
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2020-09-14 00:49:00 +0300

    comity

---
+++ Survival Horror/Assets/cameraLook.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour
+{
+    public float mouseXSensitivity = 100f;
+
+    public float mouseYSensitivity = 50f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.visible = false;
+
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseXSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseYSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
